b452a6cdf6464c2905bb9970fd9655462da816ec
MikkoLuhtasaari <Mikko.Luhtasaari@gmail.com>
2017-04-04 13:57:13 +0300

    Modify MyController.java

--- backend/ProjectBallShop/src/main/java/fi/tamk/tiko/MyController.java
+++ backend/ProjectBallShop/src/main/java/fi/tamk/tiko/MyController.java
@@ -1,22 +1,36 @@
-
-
 package fi.tamk.tiko;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
 
-// This class acts as a controller.
-// Usually when using @Controller, you will use also @RequestMapping
-@Controller
+@RestController
 public class MyController {
-    // When HTTP GET, POST, PUT or OTHER request happens
-    // to http://localhost:8080/greeting
-    // invoke this method
-    @RequestMapping("/greeting")
-    // The return value will be the HTTP Body
-    @ResponseBody
-    public String greeting() {
-        return "Test";
+    @Autowired
+    FootballRepository fbRepository;
+    
+    
+    @RequestMapping(value = "/football",  method=RequestMethod.POST)
+    public void saveLocations(@RequestBody Football football) {
+        fbRepository.save(football);
+    }
+    
+    
+    @RequestMapping(value = "/footballs",  method=RequestMethod.GET)
+    public Iterable<Football> fetchFootballs() {
+        return fbRepository.findAll();
+    }
+    
+    @RequestMapping(value = "/football/{footballId}",  method=RequestMethod.GET)
+    public Football fetchFootballs(@PathVariable long footballId) {
+        return fbRepository.findOne(footballId);
     }
 }
