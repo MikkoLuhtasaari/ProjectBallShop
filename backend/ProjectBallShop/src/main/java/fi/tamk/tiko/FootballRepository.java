37370b16c21e7960cecc032ae206099e5fafbf69
MikkoLuhtasaari <Mikko.Luhtasaari@gmail.com>
2017-04-04 13:43:41 +0300

    Create FootballRepository

---
+++ backend/ProjectBallShop/src/main/java/fi/tamk/tiko/FootballRepository.java
@@ -0,0 +1,9 @@
+
+package fi.tamk.tiko;
+
+import org.springframework.data.repository.CrudRepository;
+import java.util.List;
+
+public interface FootballRepository extends CrudRepository<Football, Long> {
+
+}
