c8a5cbb671d7b2768d6a6a22b530b26261962e67
MikkoLuhtasaari <Mikko.Luhtasaari@gmail.com>
2017-04-04 19:28:05 +0300

    Add more test data

--- backend/ProjectBallShop/src/main/java/fi/tamk/tiko/MyController.java
+++ backend/ProjectBallShop/src/main/java/fi/tamk/tiko/MyController.java
@@ -20,8 +20,16 @@ public class MyController {
     @RequestMapping(value = "/populate",  method=RequestMethod.GET)
     public String populate() {
         Football football1 = new Football("Placeholder1", "Red", 12, 500, "None", "Rubber", 1);
+        Football football2 = new Football("Placeholder2", "Green", 15, 1000, "Some details", "Rubber", 2);
+        Football football3 = new Football("Placeholder3", "Yellow", 13, 3000, "Crappy ball", "Duck skin", 3);
+        Football football4 = new Football("Placeholder4", "Red", 22, 2000, "None", "Lamb skin", 4);
+        Football football5 = new Football("Placeholder5", "Red", 25, 1000, "None", "Plastic", 5);
         fbRepository.save(football1);
-        return "Populate";
+        fbRepository.save(football2);
+        fbRepository.save(football3);
+        fbRepository.save(football4);
+        fbRepository.save(football5);
+        return "Populate succesfull";
     }
     
     
