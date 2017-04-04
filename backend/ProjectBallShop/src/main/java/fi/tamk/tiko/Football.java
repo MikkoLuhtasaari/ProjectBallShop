a0708c45ab98b4fee456b4451f00e9384bce0239
MikkoLuhtasaari <Mikko.Luhtasaari@gmail.com>
2017-04-04 13:29:10 +0300

    Create setters and getters for Football

--- backend/ProjectBallShop/src/main/java/fi/tamk/tiko/Football.java
+++ backend/ProjectBallShop/src/main/java/fi/tamk/tiko/Football.java
@@ -18,4 +18,51 @@ public class Football
     
     public Football(){};
     
+    public Football(String color, int diameter, int weigth, String details, String material, long id) {
+        setColor(color);
+        setDiameter(diameter);
+        setWeight(weight);
+        setDetails(details);
+        setMaterial(material);
+        setId(id);
+    }
+    
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public void setDiameter(int diameter) {
+        this.diameter = diameter;
+    }
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+    public void setDetails(String details) {
+        this.details = details;
+    }
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    
+    public String getColor() {
+        return color;
+    }
+    public int getDiameter() {
+        return diameter;
+    }
+    public int getWeigth() {
+        return weight;
+    }
+    public String getDetails() {
+        return details;
+    }
+    public String getMaterial() {
+        return material;
+    }
+    public long getId() {
+        return id;
+    }
+    
 }
