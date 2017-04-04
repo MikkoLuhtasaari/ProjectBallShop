0bb24a3578cd20d82bdecb67781d26166c64ab6c
MikkoLuhtasaari <Mikko.Luhtasaari@gmail.com>
2017-04-04 13:02:44 +0300

    Add starter files

---
+++ backend/ProjectBallShop/src/main/java/fi/tamk/tiko/MyController.java
@@ -0,0 +1,22 @@
+
+
+package fi.tamk.tiko;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+// This class acts as a controller.
+// Usually when using @Controller, you will use also @RequestMapping
+@Controller
+public class MyController {
+    // When HTTP GET, POST, PUT or OTHER request happens
+    // to http://localhost:8080/greeting
+    // invoke this method
+    @RequestMapping("/greeting")
+    // The return value will be the HTTP Body
+    @ResponseBody
+    public String greeting() {
+        return "Hello World";
+    }
+}
