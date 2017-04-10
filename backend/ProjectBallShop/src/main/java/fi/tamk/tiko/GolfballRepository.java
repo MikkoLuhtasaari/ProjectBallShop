ac53338d3e03827e348061c3da909a36d5242a6b
MikkoLuhtasaari <Mikko.Luhtasaari@gmail.com>
2017-04-10 18:29:22 +0300

    Create class GolfballRepository

---
+++ backend/ProjectBallShop/src/main/java/fi/tamk/tiko/GolfballRepository.java
@@ -0,0 +1,8 @@
+
+package fi.tamk.tiko;
+
+import org.springframework.data.repository.CrudRepository;
+import java.util.List;
+
+public interface GolfballRepository extends CrudRepository<Golfball, Long> {
+}
