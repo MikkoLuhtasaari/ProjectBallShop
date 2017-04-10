050928d24c88ea5f61637318461c4a5cfeca7465
MikkoLuhtasaari <Mikko.Luhtasaari@gmail.com>
2017-04-10 19:10:06 +0300

    Add imports

--- backend/ProjectBallShop/src/main/java/fi/tamk/tiko/Football.java
+++ backend/ProjectBallShop/src/main/java/fi/tamk/tiko/Football.java
@@ -4,6 +4,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.Table;
 
 @Entity
 public class Football 
