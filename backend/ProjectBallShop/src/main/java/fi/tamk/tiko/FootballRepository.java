27a66de11adf98ad81de57ebecdfe1dca6aee8be
MikkoLuhtasaari <Mikko.Luhtasaari@gmail.com>
2017-04-10 11:02:06 +0300

    Add findByColor

--- backend/ProjectBallShop/src/main/java/fi/tamk/tiko/FootballRepository.java
+++ backend/ProjectBallShop/src/main/java/fi/tamk/tiko/FootballRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface FootballRepository extends CrudRepository<Football, Long> {
-
+    List<Football> findByColor(String color);
 }
