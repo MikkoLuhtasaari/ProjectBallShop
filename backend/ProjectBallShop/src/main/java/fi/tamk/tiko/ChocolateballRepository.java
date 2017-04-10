3aa8e1464f66af40f240187b51b3ba708692bc4f
MikkoLuhtasaari <Mikko.Luhtasaari@gmail.com>
2017-04-10 12:11:44 +0300

    Create class ChocolateballRepository

---
+++ backend/ProjectBallShop/src/main/java/fi/tamk/tiko/ChocolateballRepository.java
@@ -0,0 +1,7 @@
+package fi.tamk.tiko;
+
+import org.springframework.data.repository.CrudRepository;
+import java.util.List;
+
+public interface ChocolateballRepository extends CrudRepository<Chocolateball, Long> {
+}
