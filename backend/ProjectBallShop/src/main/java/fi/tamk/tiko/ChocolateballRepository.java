f51493383959749547f2e029a113c41d3deab893
MikkoLuhtasaari <Mikko.Luhtasaari@gmail.com>
2017-04-10 13:11:55 +0300

    Add findByName

--- backend/ProjectBallShop/src/main/java/fi/tamk/tiko/ChocolateballRepository.java
+++ backend/ProjectBallShop/src/main/java/fi/tamk/tiko/ChocolateballRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface ChocolateballRepository extends CrudRepository<Chocolateball, Long> {
+    Chocolateball findByName(String name);
 }
