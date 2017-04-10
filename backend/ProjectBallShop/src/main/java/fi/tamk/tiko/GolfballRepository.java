904b511f7529d9a2f395c3396799fb6762a0e83b
MikkoLuhtasaari <Mikko.Luhtasaari@gmail.com>
2017-04-10 18:38:05 +0300

    Add findbymaterial and findbyname

--- backend/ProjectBallShop/src/main/java/fi/tamk/tiko/GolfballRepository.java
+++ backend/ProjectBallShop/src/main/java/fi/tamk/tiko/GolfballRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface GolfballRepository extends CrudRepository<Golfball, Long> {
+    List<Golfball> findByMaterial(String material);
+    Golfball findByName(String name);
 }
