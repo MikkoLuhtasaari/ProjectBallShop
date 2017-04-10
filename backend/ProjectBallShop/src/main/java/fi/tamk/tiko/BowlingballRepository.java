8e9f9400b27c65993283abf81055a31c36f3cafe
MikkoLuhtasaari <Mikko.Luhtasaari@gmail.com>
2017-04-10 11:38:36 +0300

    Add findByName

--- backend/ProjectBallShop/src/main/java/fi/tamk/tiko/BowlingballRepository.java
+++ backend/ProjectBallShop/src/main/java/fi/tamk/tiko/BowlingballRepository.java
@@ -7,4 +7,5 @@ import java.util.List;
 public interface BowlingballRepository extends CrudRepository<Bowlingball, Long> {
     List<Bowlingball> findByColor(String color);
     List<Bowlingball> findByMaterial(String material);
+    Bowlingball findByName(String name);
 }
