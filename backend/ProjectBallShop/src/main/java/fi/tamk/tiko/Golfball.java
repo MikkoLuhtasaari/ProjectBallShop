5aab4267aa16b9337d5b6b8426ba7805392226df
MikkoLuhtasaari <Mikko.Luhtasaari@gmail.com>
2017-04-10 18:47:50 +0300

    Add imports

--- backend/ProjectBallShop/src/main/java/fi/tamk/tiko/Golfball.java
+++ backend/ProjectBallShop/src/main/java/fi/tamk/tiko/Golfball.java
@@ -4,6 +4,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.Table;
 
 @Entity
 @Table(indexes = {@Index(columnList="name"), @Index(columnList="material")})
