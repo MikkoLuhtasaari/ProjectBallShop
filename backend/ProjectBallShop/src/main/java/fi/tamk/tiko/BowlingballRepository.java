61e0906b1efec9e2a9684814f7186dead4d68b00
MikkoLuhtasaari <Mikko.Luhtasaari@gmail.com>
2017-04-10 11:23:53 +0300

    Create BowlingballRepository

---
+++ backend/ProjectBallShop/src/main/java/fi/tamk/tiko/BowlingballRepository.java
@@ -0,0 +1,9 @@
+
+package fi.tamk.tiko;
+
+import org.springframework.data.repository.CrudRepository;
+import java.util.List;
+
+public interface BowlingballRepository extends CrudRepository<Bowlingball, Long> {
+
+}
