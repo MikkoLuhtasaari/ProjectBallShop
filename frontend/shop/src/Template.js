6664686fda45767075dd1962007440594a6c66cf
himpskatti <sofia.piekkola@hotmail.com>
2017-04-11 10:55:36 +0300

    Create Template.js that forwards component to be rendered

---
+++ frontend/shop/src/Template.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Footballs from './components/Footballs';
+
+export default class Template extends React.Component{
+    render(){
+        return (
+        <Footballs/>
+        );
+    }
+}
