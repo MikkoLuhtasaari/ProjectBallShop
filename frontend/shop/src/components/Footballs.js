a2d0cd78c07c944b36cab09006670f7ba992d761
himpskatti <sofia.piekkola@hotmail.com>
2017-04-11 15:02:24 +0300

    Add styling to Footballs component

--- frontend/shop/src/components/Footballs.js
+++ frontend/shop/src/components/Footballs.js
@@ -21,13 +21,24 @@ export default class Footballs extends React.Component{
 
     static createContent(fbObject) {
         const propArray = [];
-        for (const property in fbObject) {
+        const div_style = {
+            float: "left",
+            display: "inline-block",
+            padding: 20,
+            width: "20%",
+            textAlign: "center"
+        };
+        const img_style = {
+            width: "100%"
+        };
             propArray.push(
-                <div className="ball">
-                    {property}: {fbObject[property]}
+                <div class="ball_floated_img" style={div_style}>
+                    <img src="../../images/Foot.jpg" alt="Ball" style={img_style} />
+                    <h4>{fbObject.name} {fbObject.color}</h4>
+                    <h6>{fbObject.details}</h6>
+                    <h2>15,95€</h2>
                 </div>
             )
-        }
         return propArray;
     }
 }
