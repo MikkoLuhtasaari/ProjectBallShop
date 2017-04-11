266fbccaf14e48ec88466edacd9d72f63087cae3
himpskatti <sofia.piekkola@hotmail.com>
2017-04-11 15:56:38 +0300

    Add bowlingballs, chocolateballs and golfballs to template.

--- frontend/shop/src/Template.js
+++ frontend/shop/src/Template.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import Footballs from './components/Footballs';
+import Bowlingballs from './components/Bowlingballs'
+import Chocolateballs from './components/Chocolateballs'
+import Golfballs from './components/Golfballs'
 
 export default class Template extends React.Component{
     render(){
         return (
-        <Footballs/>
+            <div>
+                <Bowlingballs/>
+                <Footballs/>
+                <Chocolateballs/>
+                <Golfballs/>
+            </div>
         );
     }
 }
