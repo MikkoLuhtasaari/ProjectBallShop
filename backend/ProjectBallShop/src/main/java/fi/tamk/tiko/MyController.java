d47f876dce282cffcac001efd2672df3b9021c81
MikkoLuhtasaari <Mikko.Luhtasaari@gmail.com>
2017-04-11 18:58:52 +0300

    Update populate data

--- backend/ProjectBallShop/src/main/java/fi/tamk/tiko/MyController.java
+++ backend/ProjectBallShop/src/main/java/fi/tamk/tiko/MyController.java
@@ -30,9 +30,11 @@ public class MyController {
         //FOOTBALLS
         GoalSportsBall tempGoal1 = new GoalSportsBall("Football1", "Black", 50, 300, "Somethingsomething", "Rubber", "Adidas", "A ball to kick", "Football", 30.95, 3, 1);
         GoalSportsBall tempGoal2 = new GoalSportsBall("Football2", "Orange", 40, 200, "LongDetails", "Rubber", "AnotherDas", "A ball not to kick", "Football", 105.95, 4, 2);
+        GoalSportsBall tempGoal3 = new GoalSportsBall("Bad Basketball", "White", 40, 20000, "LongDetails", "Concrete", "AnotherDas", "A ball to throw", "Basketball", 19.95, 4, 3);
         
         gsbRepository.save(tempGoal1);
         gsbRepository.save(tempGoal2);
+        gsbRepository.save(tempGoal3);
         
         //BOWLINGBALLS & GOLFBALLS
         TargetSportsBall tempTarget1 = new TargetSportsBall("Bowlingball1", "Black", 40, 3030, "Somethingsomething", "Steel", "Adidas", "A ball not to kick", "Bowlingball", 109.95, 3, 1);
@@ -46,8 +48,51 @@ public class MyController {
         return "Populate succesfull";
     }
     
+    // Football related stuff
+    @RequestMapping(value = "/targetsportsball",  method=RequestMethod.POST)
+    public void saveTargetSportsBall(@RequestBody TargetSportsBall targetsportsball) {
+        tsbRepository.save(targetsportsball);
+    }
+    
+    @RequestMapping(value = "/targetsportsballs",  method=RequestMethod.GET)
+    public Iterable<TargetSportsBall> fetchTargetSportsBalls() {
+        return tsbRepository.findAll();
+    }
+    
+    @RequestMapping(value = "/targetsportsball/{targetsportsballId}",  method=RequestMethod.GET)
+    public TargetSportsBall fetchTargetSportsBall(@PathVariable long targetsportsballId) {
+        return tsbRepository.findOne(targetsportsballId);
+    }
+    
+    @RequestMapping(value = "/targetsportsball/{targetsportsballId}",  method=RequestMethod.DELETE)
+    public TargetSportsBall deleteTargetSportsBall(@PathVariable long targetsportsballId) {
+        TargetSportsBall temp = tsbRepository.findOne(targetsportsballId);
+        tsbRepository.delete(tsbRepository.findOne(targetsportsballId));
+        return temp;
+    }
+    
+    @RequestMapping(value = "/targetsportsball/color/{targetsportsballColor}",  method=RequestMethod.GET)
+    public List<TargetSportsBall> fetchTargetSportsBallsByColor(@PathVariable String targetsportsballColor) {
+        return tsbRepository.findByColor(targetsportsballColor);
+    }
+    
+    @RequestMapping(value = "/targetsportsball/material/{targetsportsballMaterial}",  method=RequestMethod.GET)
+    public List<TargetSportsBall> fetchTargetSportsBallsByMaterial(@PathVariable String targetsportsballMaterial) {
+        return tsbRepository.findByMaterial(targetsportsballMaterial);
+    }
+    
+    @RequestMapping(value = "/targetsportsball/name/{bowlingballName}",  method=RequestMethod.GET)
+    public TargetSportsBall fetchTargetSportsBallByName(@PathVariable String targetsportsballName) {
+        return tsbRepository.findByName(targetsportsballName);
+    }
+     
+    @RequestMapping(value = "/targetsportsball/type/{type}",  method=RequestMethod.GET)
+    public List<TargetSportsBall> fetchTargetSportsBallsByType(@PathVariable String type) {
+        return tsbRepository.findByType(type);
+    }
+    
 
-    // Bowlingball related stuff
+    // Bowlingball and Golfball related stuff
     @RequestMapping(value = "/targetsportsball",  method=RequestMethod.POST)
     public void saveTargetSportsBall(@RequestBody TargetSportsBall targetsportsball) {
         tsbRepository.save(targetsportsball);
