ab666db9e526ae71080ed1727a17f32a3bc815d8
himpskatti <sofia.piekkola@hotmail.com>
2017-04-11 15:54:55 +0300

    Add initial content to Golfballs

--- frontend/shop/src/components/Golfballs.js
+++ frontend/shop/src/components/Golfballs.js
@@ -1,3 +1,44 @@
-/**
- * Created by Sofia on 11.4.2017.
- */
+import React from 'react'; import Client from '../Client'
+
+export default class Golfballs extends React.Component{
+
+    constructor(props) {
+        super(props);
+        this.client = new Client();
+        this.state = {golfballs : []};
+        this.client.golfballs().then(g => this.setState({golfballs:g}));
+    }
+
+    render(){
+        return(
+            <section>
+                {
+                    this.state.golfballs.map(gb => Golfballs.createContent(gb))
+                }
+            </section>
+        )
+    }
+
+    static createContent(gbObject) {
+        const propArray = [];
+        const div_style = {
+            float: "left",
+            display: "inline-block",
+            padding: 20,
+            width: "20%",
+            textAlign: "center"
+        };
+        const img_style = {
+            width: "100%"
+        };
+        propArray.push(
+            <div style={div_style}>
+                <img src="../../images/Golf.png" alt="Ball" style={img_style} />
+                <h4>{gbObject.name} {gbObject.color}</h4>
+                <h6>{gbObject.details}</h6>
+                <h2>15,95€</h2>
+            </div>
+        )
+        return propArray;
+    }
+}
