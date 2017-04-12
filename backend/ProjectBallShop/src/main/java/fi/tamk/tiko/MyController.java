4c39bdbde5155eeac7d1f7ede9411b81c6ec1c20
MikkoLuhtasaari <Mikko.Luhtasaari@gmail.com>
2017-04-12 13:36:53 +0300

    Populate at app start

--- backend/ProjectBallShop/src/main/java/fi/tamk/tiko/MyController.java
+++ backend/ProjectBallShop/src/main/java/fi/tamk/tiko/MyController.java
@@ -46,7 +46,40 @@ public class MyController implements ApplicationRunner {
         System.out.println("                                                   ");
         System.out.println("                                                   ");
         
+        GoalSportsBall tempGoal1 = new GoalSportsBall("Football1", "Black", 50, 300, "Somethingsomething", "Rubber", "Adidas", "A ball to kick", "Football", 30.95, 3, 1);
+        GoalSportsBall tempGoal2 = new GoalSportsBall("Football2", "Orange", 40, 200, "LongDetails", "Rubber", "AnotherDas", "A ball not to kick", "Football", 105.95, 4, 2);
+        GoalSportsBall tempGoal3 = new GoalSportsBall("Bad Basketball", "White", 40, 20000, "LongDetails", "Concrete", "AnotherDas", "A ball to throw", "Basketball", 19.95, 4, 3);
+        
+        gsbRepository.save(tempGoal1);
+        gsbRepository.save(tempGoal2);
+        gsbRepository.save(tempGoal3);
+        
+        //BOWLINGBALLS & GOLFBALLS
+        TargetSportsBall tempTarget1 = new TargetSportsBall("Bowlingball1", "Black", 40, 3030, "Somethingsomething", "Steel", "Adidas", "A ball not to kick", "Bowlingball", 109.95, 3, 1);
+        TargetSportsBall tempTarget2 = new TargetSportsBall("Golfball1", "Orange", 40, 200, "", "Rubber", "AnotherDas", "A ball not to kick", "Golfball", 105.95, 4, 2);
+        TargetSportsBall tempTarget3 = new TargetSportsBall("bb3", "Grey", 40, 6000, "Heavy ball", "Concrete", "Adidas", "A ball not to kick", "Bowlingball", 102.95, 5, 3);
+        
+        tsbRepository.save(tempTarget1);
+        tsbRepository.save(tempTarget2);
+        tsbRepository.save(tempTarget3);
+        
+        //BASEBALLS & TENNISBALLS
+        BatAndRaquetsGames tempBat1 = new BatAndRaquetsGames("Baseball1", "Black", 5, 30, "Somethingsomething", "Rubber", "Adidas", "A ball to throw", "Baseball", 4.95, 3, 1);
+        BatAndRaquetsGames tempBat2 = new BatAndRaquetsGames("Baseball2", "White", 6, 35, "Somethingsomething", "Elastic rubber", "Adidas", "A ball to throw2", "Baseball", 4.95, 3, 2);
+        BatAndRaquetsGames tempBat3 = new BatAndRaquetsGames("Tennisball1", "Yellow", 5, 336, "Somethingsomething", "Rubber", "Adidas", "A ball to smash", "Tennisball", 2.95, 3, 3);
 
+        brRepository.save(tempBat1);
+        brRepository.save(tempBat2);
+        brRepository.save(tempBat3);
+        
+        // VOLLEYBALL & HANDBALL
+        NetSportsBall tempNS1 = new NetSportsBall("Volleyball1", "Black", 5, 30, "Somethingsomething", "Rubber", "Adidas", "A ball to throw", "Volleyball", 4.95, 3, 1);
+        NetSportsBall tempNS2 = new NetSportsBall("Volleyball2", "White", 50, 300, "Somethingsomeasdthing", "Rubber", "Adidas", "A ball to throw", "Volleyball", 8.95, 3, 2);
+        NetSportsBall tempNS3 = new NetSportsBall("Handball1", "Black", 5, 360, "Somethingsomething", "Rubber", "Adidas", "A ball to throw", "Handball", 4.95, 3, 3);
+        
+        nsRepository.save(tempNS1);
+        nsRepository.save(tempNS2);
+        nsRepository.save(tempNS3);
     }
     
     
