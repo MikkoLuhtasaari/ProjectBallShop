0559650e9572471f1f8168ceaddd0edc6685b19d
MikkoLuhtasaari <Mikko.Luhtasaari@gmail.com>
2017-04-12 13:29:57 +0300

    Organize imports

--- backend/ProjectBallShop/src/main/java/fi/tamk/tiko/MyController.java
+++ backend/ProjectBallShop/src/main/java/fi/tamk/tiko/MyController.java
@@ -3,14 +3,17 @@ package fi.tamk.tiko;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.ApplicationArguments;
 
