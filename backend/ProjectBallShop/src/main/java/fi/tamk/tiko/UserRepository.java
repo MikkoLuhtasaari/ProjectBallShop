7518214b8dcbab5886861ee001b44bc2c417610e
MikkoLuhtasaari <Mikko.Luhtasaari@gmail.com>
2017-04-23 15:49:48 +0300

    Rename method

--- backend/ProjectBallShop/src/main/java/fi/tamk/tiko/UserRepository.java
+++ backend/ProjectBallShop/src/main/java/fi/tamk/tiko/UserRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface UserRepository extends CrudRepository<User , Long> {
-    User findByName(String name);
+    User findByUserName(String userName);
 }
