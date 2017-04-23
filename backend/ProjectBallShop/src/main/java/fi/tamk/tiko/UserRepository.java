2fca439d1645661b1a2eebf534d0438fbde76233
MikkoLuhtasaari <Mikko.Luhtasaari@gmail.com>
2017-04-23 15:47:11 +0300

    Add method

--- backend/ProjectBallShop/src/main/java/fi/tamk/tiko/UserRepository.java
+++ backend/ProjectBallShop/src/main/java/fi/tamk/tiko/UserRepository.java
@@ -3,4 +3,6 @@ package fi.tamk.tiko;
 import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
-public interface UserRepository extends CrudRepository<User , Long> {}
+public interface UserRepository extends CrudRepository<User , Long> {
+    User findByName(String name);
+}
