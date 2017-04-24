9271e8cd470f9ca1d5e510b1571e70a792d33c81
himpskatti <sofia.piekkola@hotmail.com>
2017-04-24 21:51:56 +0300

    Add real-time feature to search

--- frontend/shop/src/components/SearchComponent.js
+++ frontend/shop/src/components/SearchComponent.js
@@ -1,21 +1,61 @@
 import React from 'react';
-import Client from '../Client';
+import Client from "../Client";
 
-export default class SearchComponent extends React.Component{
+export default class SearchComponent extends React.Component {
+    state = { searchString: '', balls: [] };
+    handleChange = (e) => {
+        this.setState({searchString: e.target.value});
+    };
+    keynum = 0;
+    ballsArray = [];
+
     constructor(props) {
         super(props);
         this.client = new Client();
-        this.state = {
-            updated:false
-        };
+
+        this.client.balls("netsportsballs").then(b => this.setState({balls: b}));
+        this.client.balls("batandraquetsgames").then(b => this.setState({balls: b}));
+        this.client.balls("goalsportsballs").then(b => this.setState({balls: b}));
+        this.client.balls("targetsportsballs").then(b => this.setState({balls: b}));
     }
 
-    render(){
-        return(
-            <div className="input-group">
-                <input type="text" className="search-box" placeholder="Search"/>
-                <button type="submit" className="btn"><span className="glyphicon glyphicon-search"/></button>
-            </div>
-        )
+    render() {
+        let tempArray = this.ballsArray;
+        this.state.balls.map(b => this.ballsArray.push(b));
+        let searchString = this.state.searchString;
+        if (searchString.length > 0){
+            tempArray = this.ballsArray.filter(function (i) {
+                if (i.name.toLowerCase().match(searchString.toLocaleLowerCase())) return true;
+                else return !!i.manufacturer.toLowerCase().match(searchString.toLowerCase());
+            });
+        }
+        return this.toReturn(searchString.length, tempArray, this.keynum);
+    }
+
+    toReturn(length, tempArray, keynum) {
+        if (length > 0) {
+            return (
+                <div key={"dsa"} className="input-group">
+                    <input type="text" value={this.state.searchString} onChange={this.handleChange}
+                           className="search-box" placeholder="Search"/>
+                    <button type="submit" className="btn"><span className="glyphicon glyphicon-search"/></button>
+                    <ul>
+                        {tempArray.map(function (i) {
+                            keynum++;
+                            let url = "/#/" + i.type + "/" + i.id;
+                            return <li key={keynum} className="list-unstyled"><a href={url}>{i.manufacturer} {i.name}</a></li>;
+                        })}
+                    </ul>
+                </div>
+            )
+        }else {
+            return (
+                <div key={"dsa"} className="input-group">
+                    <input type="text" value={this.state.searchString} onChange={this.handleChange}
+                           className="search-box" placeholder="Search"/>
+                    <button type="submit" className="btn"><span className="glyphicon glyphicon-search"/></button>
+                </div>
+            );
+        }
     }
 }
