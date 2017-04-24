1078e007ed9e796912c56dc0c091a385b7c16d29
himpskatti <sofia.piekkola@hotmail.com>
2017-04-24 18:20:31 +0300

    Add methods for adding and removing items

--- frontend/shop/src/components/ShoppingCartComponent.js
+++ frontend/shop/src/components/ShoppingCartComponent.js
@@ -1,40 +1,81 @@
 import React from 'react';
-import Client from '../Client';
 
 export default class ShoppingCartComponent extends React.Component{
     constructor(props) {
         super(props);
-        this.client = new Client();
         this.state = {
-            updated:false
+            items: [],
+            total: 0,
+            currency: 'EUR'
         };
     }
 
-    render(){
+    render() {
+        console.log(this.state.items);
+        const items = this.state.items.map(function (item) {
+            return (
+                ShoppingCartComponent.content(item)
+            )
+        });
+        const body = (<ul>{items}</ul>);
+        //const empty = <div>Cart is empty</div>;
+        const empty = ShoppingCartComponent.content();
+        return ((items.length > 0 ? body : empty))
+    }
+
+    addItem(obj, item) {
+        this.state.items.push(item);
+        this.countTotal();
+    }
+
+    removeItem(obj, itemId) {
+        let itemIndex;
+        this.state.items.some(function (item, index) {
+            if (item.id === itemId) {
+                itemIndex = index;
+                return true;
+            }
+            return false;
+        });
+
+        if (typeof itemId !== 'undefined') {
+            this.state.items.splice(itemIndex, 1);
+        }
+    }
+
+    countTotal() {
+        let total = 0;
+        this.state.items.forEach(function (item, index) {
+            total += item.price;
+        });
+        this.setState({total: total})
+    }
+
+    static content(item) {
+        console.log(item);
         return(
-            <li className="dropdown">
-                <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                    <span className="glyphicon glyphicon-shopping-cart"/><b>7 - Items</b><span className="caret"/>
-                </a>
-                <ul className="dropdown-menu dropdown-cart" role="menu">
-                    <li>
+        <li className="dropdown">
+            <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                <span className="glyphicon glyphicon-shopping-cart"/><b>View Cart</b><span className="caret"/>
+            </a>
+            <ul className="dropdown-menu dropdown-cart" role="menu">
+                <li>
                               <span className="item">
                                 <span className="item-left">
                                     <img src="images/items/Football_1.png" id="img40" alt="item" />
                                     <span className="item-info">
-                                        <span>ITEM NAME</span>
-                                        <span>23€</span>
+                                        <span>Joku paska tuote</span>
+                                        <span>Vitusti €</span>
                                     </span>
                                 </span>
                                 <span className="item-right">
                                     <button className="btn btn-xs btn-danger pull-right" id="marginR10">x</button>
                                 </span>
                             </span>
-                    </li>
-                    <li className="divider"/>
-                    <li><a className="text-center" href="">View Cart</a></li>
-                </ul>
-            </li>
-        )
-    }
-}
+                </li>
+                <li className="divider"/>
+                <li><b><a href="">Siirry maksaan</a><p className="pull-right" href="">Total: 2500€</p></b></li>
+            </ul>
+        </li>
+    )
+};}
