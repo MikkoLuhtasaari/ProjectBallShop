91254bca72bdba2ea6702028b23c8f6ddb4f60c8
MikkoLuhtasaari <Mikko.Luhtasaari@gmail.com>
2017-04-24 14:43:56 +0300

    Create class

---
+++ backend/ProjectBallShop/src/main/java/fi/tamk/tiko/ReviewRepository.java
@@ -0,0 +1,8 @@
+
+package fi.tamk.tiko;
+
+import org.springframework.data.repository.CrudRepository;
+import java.util.List;
+
+public interface ReviewRepository extends CrudRepository<Review, Long> {
+}
