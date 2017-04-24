85037b9d943258c722ec2916edb5542944952c7d
MikkoLuhtasaari <Mikko.Luhtasaari@gmail.com>
2017-04-24 15:54:39 +0300

    Add populate data

--- backend/ProjectBallShop/src/main/java/fi/tamk/tiko/MyController.java
+++ backend/ProjectBallShop/src/main/java/fi/tamk/tiko/MyController.java
@@ -112,6 +112,15 @@ public class MyController implements ApplicationRunner {
         System.out.println("GET targetsportsball/color/{color}");
         System.out.println("GET targetsportsball/type/{Football | Basketball}");
         
+        // REVIEWS
+        Review tempReview1 = new Review("Bat and Racquet games", 1, 1, 2, "Header", "Content", 1);
+        Review tempReview2 = new Review("Goal sports", 1, 2, 5, "Header2", "Content2", 2);
+        Review tempReview3 = new Review("Bat and Racquet games", 2, 1, 3, "Header3", "Content3", 3);
+        
+        reviewRepository.save(tempReview1);
+        reviewRepository.save(tempReview2);
+        reviewRepository.save(tempReview3);
+        
         //USERS
         User tempUser1 = new User("Jeppe", "Jeppenen", "Jeppetes", "salasana", "jeppe@jeppe.com", "Tampere", "Ruhtinaankatu 1", 33560, "Admin", 1);
         User tempUser2 = new User("Jaska", "Jokunen", "MirrinSurma", "salasana123", "jaska@jeppe.com", "Vaasa", "Slottintie 19", 65220, "User", 2);
