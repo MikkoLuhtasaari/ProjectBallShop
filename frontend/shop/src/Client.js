55cec1fe8b381cf23e77c0f08eb307e7fa8b01dd
himpskatti <sofia.piekkola@hotmail.com>
2017-05-18 01:37:59 +0300

    Fix method calls to more simplified form
    Create one method that is used in all requests.

--- frontend/shop/src/Client.js
+++ frontend/shop/src/Client.js
@@ -1,56 +1,40 @@
-export default class Client{
+export default class Client {
 
-    ballsByName(sporttype, balltype){
-        return new Promise((resolve, reject) => {
-            let request = new XMLHttpRequest();
-            request.open("GET", "http://localhost:8080/" + sporttype + "/type/" + balltype);
-            request.onreadystatechange = () => {
-                if (request.readyState === 4 && request.status === 200) {
-                    let raw = request.responseText;
-                    let objectified = JSON.parse(raw);
-                    resolve(objectified);
-                }
-            };
-            request.send();
-        });
+    /**
+     * Gets all balls from a specific sport type
+     *
+     * Type can be netsportsballs, batandraquetsgames, goalsportsballs or targetsportsballs.
+     *
+     * @param sporttype type of balls to be retrieved.
+     * @returns {Promise}
+     */
+    ballsByType(sporttype) {
+        return this.getPromise("GET", "http://localhost:8080/" + sporttype);
     }
 
-    ballsByType(sporttype){
-        return new Promise((resolve, reject) => {
-            let request = new XMLHttpRequest();
-            request.open("GET", "http://localhost:8080/" + sporttype);
-            request.onreadystatechange = () => {
-                if (request.readyState === 4 && request.status === 200) {
-                    let raw = request.responseText;
-                    let objectified = JSON.parse(raw);
-                    resolve(objectified);
-                }
-            };
-            request.send();
-        });
+    ballsByName(sporttype, balltype) {
+        return this.getPromise("GET", "http://localhost:8080/" + sporttype + "/type/" + balltype);
     }
 
-    ballById(sporttype, id){
-        return new Promise((resolve, reject) => {
-            let request = new XMLHttpRequest();
-            request.open("GET", "http://localhost:8080/" + sporttype + "/" + id);
-            request.onreadystatechange = () => {
-                if (request.readyState === 4 && request.status === 200) {
-                    let raw = request.responseText;
-                    let objectified = JSON.parse(raw);
-                    resolve(objectified);
-                }
-            };
-            request.send();
-        });
+    ballById(sporttype, id) {
+        return this.getPromise("GET", "http://localhost:8080/" + sporttype + "/" + id);
     }
 
-    reviewByBallId(sporttype, ballId){
+    /**
+     * Gets reviews for specific ball
+     *
+     * @param sporttype - type of sport that the ball that's reviews are to be retrieved represents
+     * @param ballId - id of the ball that's reviews are to be retrieved
+     * @returns {Promise}
+     */
+    reviewByBallId(sporttype, ballId) {
+        return this.getPromise("GET", "http://localhost:8080/review/" + sporttype + "/" + ballId);
+    }
+
+    getPromise(type, address) {
         return new Promise((resolve, reject) => {
             let request = new XMLHttpRequest();
-            let link = "http://localhost:8080/review/" + sporttype + "/" + ballId;
-            request.open("GET", link);
-            console.log();
+            request.open(type, address);
             request.onreadystatechange = () => {
                 if (request.readyState === 4 && request.status === 200) {
                     let raw = request.responseText;
