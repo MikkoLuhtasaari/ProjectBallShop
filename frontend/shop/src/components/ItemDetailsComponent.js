f6afdf7650b11ad8666e09eac6025c4e9104c9ee
himpskatti <sofia.piekkola@hotmail.com>
2017-05-19 00:59:36 +0300

    Divide to multiple methods and add correct calls for reviews

--- frontend/shop/src/components/ItemDetailsComponent.js
+++ frontend/shop/src/components/ItemDetailsComponent.js
@@ -7,75 +7,27 @@ export default class ItemDetailsComponent extends React.Component {
     constructor(props) {
         super(props);
         this.client = new Client();
-        this.state = {ball: ''};
+        this.state = { ball: '' };
         this.client.ballById(this.props.params.group, this.props.params.id).then(b => this.setState({ball: b}));
     }
 
+    //Todo Tätä returnia kutsutaan liian aikaisin jonka takia ReviewsComponentiin lähtee undefindeja.
+    // Todo Pystyykö renderiä kutsuun vasta kun clientiltä on tullu vastaus TAI kutsuun sitä uudestaan kun clientiltä on tullu vastaus?
     render() {
-        const ball = this.state.ball;
-        let category = ball.category;
-        if (typeof category !== "undefined") category = category.replace(/ /g, '').toLowerCase();
-        let imageSrc = "../../images/items/" + ball.type + "_" + ball.id + ".png";
-        let onStock = "This item is out of stock";
-        let icon = "glyphicon glyphicon-remove-circle";
-        let colorId = "red";
-        let buttonId = "btn disabled";
-        if (ball.amount > 0) {
-            onStock = "On stock";
-            icon = "glyphicon glyphicon-ok-circle";
-            colorId = "green";
-            buttonId = "btn btn-success active";
-        }
+        let imageSrc = "../../images/items/" + this.state.ball.type + "_" + this.state.ball.id + ".png";
         return (
             <section>
                 {
                     <div>
                         <div className="row">
                             <div className="col-xs-1"/>
-
                             <div className="col-xs-3 item-photo">
                                 <img alt="item" id="wideImg" src={imageSrc}/>
                             </div>
                             <div className="col-xs-1"/>
-                            <div className="col-xs-3 text-center">
-                                <h3>{ball.manufacturer} {ball.type}</h3>
-                                <h5 id="padBot">{ball.shortDetails}</h5>
-                                <h3>{ball.price} €</h3>
-
-                                <div className="section" id="botBad">
-                                    <button className={buttonId} onClick={ () => this.addCookie(ball) }>
-                                        <span id="marginR20" className="glyphicon glyphicon-shopping-cart" aria-hidden="true"/>
-                                        Add to cart
-                                    </button>
-                                </div>
-
-                                <span id={colorId}>
-                                    <span className={icon}/>
-                                    <span id="shopItem">{onStock}</span>
-                                </span>
-                                <br/><br/>
-                                <span className="text-left">
-                                    <ReviewsComponent group={category} ballId={ball.id} need={"light"}
-                                                      location={"tweaked-margin"}/>
-                                </span>
-
-                            </div>
+                            {this.getItemInfo()}
                             <div className="col-xs-1"/>
-                            <div className="col-xs-3">
-                                <ul>
-                                    <li id="minH50">{ball.details}</li>
-                                    <br/>
-                                    <li>Name: {ball.name}</li>
-                                    <li>Color: {ball.color}</li>
-                                    <li>Diameter: {ball.diameter}</li>
-                                    <li>Weight: {ball.weigth}</li>
-                                    <li>Name: {ball.name}</li>
-                                    <li>Manufacturer: {ball.manufacturer}</li>
-                                    <li>Category: {ball.category}</li>
-                                </ul>
-                            </div>
-
-
+                            {this.getDetailsList()}
                             <div className="thumbnail">
                                 <div className="col-xs-12" id="wideDiv"/>
                                 <div className="col-xs-1"/>
@@ -94,6 +46,62 @@ export default class ItemDetailsComponent extends React.Component {
                 }
             </section>
         )
+    }
+
+    getDetailsList(){
+        let ball = this.state.ball;
+        return (
+            <div className="col-xs-3">
+                <ul>
+                    <li id="minH50">{ball.details}</li>
+                    <br/>
+                    <li>Name: {ball.name}</li>
+                    <li>Color: {ball.color}</li>
+                    <li>Diameter: {ball.diameter}</li>
+                    <li>Weight: {ball.weigth}</li>
+                    <li>Name: {ball.name}</li>
+                    <li>Manufacturer: {ball.manufacturer}</li>
+                    <li>Category: {ball.category}</li>
+                </ul>
+            </div>
+        );
+    }
+
+    getItemInfo(){
+        let category = this.state.ball.category;
+        if (typeof category !== "undefined") category = category.replace(/ /g, '').toLowerCase();
+        let onStock = "This item is out of stock";
+        let icon = "glyphicon glyphicon-remove-circle";
+        let colorId = "red";
+        let buttonId = "btn disabled";
+        if (this.state.ball.amount > 0) {
+            onStock = "On stock";
+            icon = "glyphicon glyphicon-ok-circle";
+            colorId = "green";
+            buttonId = "btn btn-success active";
+        }
+        return (
+            <div className="col-xs-3 text-center">
+                <h3>{this.state.ball.manufacturer} {this.state.ball.type}</h3>
+                <h5 id="padBot">{this.state.ball.shortDetails}</h5>
+                <h3>{this.state.ball.price} €</h3>
+                <div className="section" id="botBad">
+                    <button className={buttonId} onClick={ () => this.addCookie(this.state.ball) }>
+                        <span id="marginR20" className="glyphicon glyphicon-shopping-cart" aria-hidden="true"/>
+                        Add to cart
+                    </button>
+                </div>
+                <span id={colorId}>
+                    <span className={icon}/>
+                    <span id="shopItem">{onStock}</span>
+                </span>
+                <br/><br/>
+                <span className="text-left">
+                    <ReviewsComponent group={category} ballId={this.state.ball.id}
+                                      need={"light"} location={"tweaked-margin"}/>
+                </span>
+            </div>
+        );
     }
 
     addCookie(ball) {
