9870eb800dded933d42d9f9271844ec000b86aba
himpskatti <sofia.piekkola@hotmail.com>
2017-05-19 21:03:46 +0300

    Fix create account route

--- frontend/shop/src/components/LoginComponent.js
+++ frontend/shop/src/components/LoginComponent.js
@@ -18,7 +18,7 @@ export default class LoginComponent extends React.Component{
             <li>
                 <div className="row">
                     <div className="col-md-12">
-                        <form className="form" role="form" method="post" action="login" id="login-nav">
+                        <form className="form" role="form" id="login-nav">
                             <div className="form-group">
                                 <label className="sr-only" htmlFor="exampleInputEmail2">Email address</label>
                                 <input type="email" className="form-control" id="exampleInputEmail2" placeholder="Email address" required/>
@@ -32,7 +32,7 @@ export default class LoginComponent extends React.Component{
                             </div>
                         </form>
                     </div>
-                    <div className="bottom text-center"><a href="http://localhost:3000/#/type/netsportsball/CreateAccount/"><b>Create new account</b></a>
+                    <div className="bottom text-center"><a href="http://localhost:3000/#/CreateAccount/"><b>Create new account</b></a>
                     </div>
                 </div>
             </li>
