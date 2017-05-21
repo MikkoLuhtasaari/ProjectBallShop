2d406a88f061096efaa7740767029abd749fa6f3
himpskatti <sofia.piekkola@hotmail.com>
2017-05-21 22:25:11 +0300

    Add basic validation for user + password

--- frontend/shop/src/components/LoginComponent.js
+++ frontend/shop/src/components/LoginComponent.js
@@ -24,8 +24,13 @@ export default class LoginComponent extends React.Component {
     }
 
     login(login) {
-        if (login) this.client.login(this.refs.user.value, this.refs.pass.value).then(this.setState({name: "Pekka Perärööri"}));
-        else this.setState({name: "Login"});
+        if (login) {
+            let pass = this.refs.pass.value;
+            let userName = this.refs.user.value;
+            this.client.userLogin(userName).then(
+                (success) => {this.validateUser(success, pass, true)},
+                (failure) => {this.validateUser(failure, pass, false)});
+        } else this.setState({name: "Login"});
     }
 
     loggedIn(logged) {
@@ -42,8 +47,7 @@ export default class LoginComponent extends React.Component {
                                 <div className="form-group">
                                     <label className="sr-only" htmlFor="exampleInputPassword2">Password</label>
                                     <input type="password" ref="pass" className="form-control"
-                                           id="exampleInputPassword2"
-                                           placeholder="Password" required/>
+                                           id="exampleInputPassword2" placeholder="Password" required/>
                                 </div>
                                 <div className="form-group">
                                     <button type="submit" data-toggle="dropdown" onClick={() => this.login(true)}
@@ -69,5 +73,13 @@ export default class LoginComponent extends React.Component {
                 </li>
             )
         }
+    }
+
+    validateUser(user, pass, success) {
+        if(!success){
+            alert("Incorrect username.\nPlease try again.")
+        } else if(pass === user.password){
+            this.setState({name: user.firstName + " " + user.lastName});
+        } else alert("Incorrect password.\nPlease try again.");
     }
 }
