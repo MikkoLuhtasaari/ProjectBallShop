5e9dd4af25baa48928a6ae56105da2ff4c663283
MikkoLuhtasaari <Mikko.Luhtasaari@gmail.com>
2017-05-22 13:42:14 +0300

    Fix formatting

--- backend/ProjectBallShop/src/main/java/fi/tamk/tiko/BatAndRaquetsGames.java
+++ backend/ProjectBallShop/src/main/java/fi/tamk/tiko/BatAndRaquetsGames.java
@@ -7,7 +7,7 @@ import java.util.Set;
 import java.io.Serializable;
 
 @Entity
-@Table(indexes = {@Index(columnList="name"), @Index(columnList="color"), @Index(columnList="amount"), @Index(columnList="material"), @Index(columnList="type")})
+@Table(indexes = {@Index(columnList="name"), @Index(columnList="amount"), @Index(columnList="type")})
 public class BatAndRaquetsGames {
     
     @Column(name = "name", length = 128, nullable = false, unique = false)
@@ -78,90 +78,108 @@ public class BatAndRaquetsGames {
     public void setName(String name) {
         this.name = name;
     }
+    
     public void setColor(String color) {
         this.color = color;
     }
+    
     public void setDiameter(int diameter) {
         this.diameter = diameter;
     }
+    
     public void setWeight(int weigth) {
         this.weigth = weigth;
     }
+    
     public void setDetails(String details) {
         this.details = details;
     }
+    
     public void setMaterial(String material) {
         this.material = material;
     }
+    
     public void setManufacturer(String manufacturer) {
         this.manufacturer = manufacturer;
     }
+    
     public void setShortDetails(String shortDetails) {
         this.shortDetails = shortDetails;
     }
+    
     public void setType(String type) {
         this.type = type;
     }
+    
     public void setPrice(double price) {
         this.price = price;
     }
+    
     public void setAmount(int amount) {
         this.amount = amount;
     }
+    
     public void setId(long id) {
         this.id = id;
     }
+    
     public void setReviews(Set<BARReview> reviews) {
-        System.out.println("Setting reviews");
-        System.out.println(reviews.size());
-        
-        for(BARReview n: reviews) {
-            System.out.println(n.getScore());
-        }
         this.reviews = reviews;
     }
     
     public String getName() {
         return name;
     }
+    
     public String getColor() {
         return color;
     }
+    
     public int getDiameter() {
         return diameter;
     }
+    
     public int getWeigth() {
         return weigth;
     }
+    
     public String getDetails() {
         return details;
     }
+    
     public String getMaterial() {
         return material;
     }
+    
     public String getManufacturer() {
         return manufacturer;
     }
+    
     public String getShortDetails() {
         return shortDetails;
     }
+    
     public String getType() {
         return type;
     }
+    
     public double getPrice() {
         return price;
     }
+    
     public int getAmount() {
         return amount;
     }
+    
     public long getId() {
         return id;
     }
+    
     public String getCategory() {
         return category;
     }
+    
     public Set<BARReview> getReviews() {
        return reviews;
     }
-    
 }
