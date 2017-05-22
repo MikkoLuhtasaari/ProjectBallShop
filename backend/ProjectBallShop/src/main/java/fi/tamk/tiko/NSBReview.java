2da9eb2ffe90b043234ae08c05e6cc8cb9f741a1
MikkoLuhtasaari <Mikko.Luhtasaari@gmail.com>
2017-05-22 13:38:06 +0300

    Fix formatting

--- backend/ProjectBallShop/src/main/java/fi/tamk/tiko/NSBReview.java
+++ backend/ProjectBallShop/src/main/java/fi/tamk/tiko/NSBReview.java
@@ -53,6 +53,7 @@ public class NSBReview implements Serializable
     public void setId(long id) {
         this.id = id;
     }
+    
     public void setScore(int score) {
         if(score < 6 && score > 0 ) {
             this.score = score;
@@ -60,52 +61,59 @@ public class NSBReview implements Serializable
             System.out.println("Score must be between 1-5");
         }
     }
+    
     public void setHeader(String header) {
         this.header = header;
     }
+    
     public void setContent(String content) {
         this.content = content;
     }
+    
     public void setOwner(NetSportsBall owner) {
         this.owner = owner;
     }
+    
     public void setUserOwner(User userOwner) {
         this.userOwner = userOwner;
     }
+    
     public void setOwnerBallId(long ownerBallId) {
         this.ownerBallId = ownerBallId;
     }
+    
     public void setUserId(long userId) {
         this.userId = userId;
     }
     
     public long getId() {
-        System.out.println("Getting review id");
         return id;
     }
+    
     public int getScore() {
         return score;
     }
+    
     public String getHeader() {
         return header;
     }
+    
     public String getContent() {
         return content;
     }
     
     public NetSportsBall getOwner() {
-        System.out.println("Get owner");
         return owner;
     }
     
     public User getUserOwner() {
-        System.out.println("Get user");
         return userOwner;
     }
     
     public long getUserId() {
         return userId;
     }
+    
     public long getOwnerBallId() {
         return ownerBallId;
     }
