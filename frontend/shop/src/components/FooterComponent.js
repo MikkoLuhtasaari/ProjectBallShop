982d41512ddab28ff0234e6899392aacb3ffd62e
himpskatti <sofia.piekkola@hotmail.com>
2017-05-24 01:00:43 +0300

    Add Footer to react

---
+++ frontend/shop/src/components/FooterComponent.js
@@ -0,0 +1,16 @@
+import * as React from "react";
+
+export default class FooterComponent extends React.Component{
+    render(){
+        return(
+            <footer className="colorGray">
+                <div className="row">
+                    <div className="col-lg-12">
+                        <p>Copyright &copy; Ball Hawks 2017</p>
+                    </div>
+                </div>
+            </footer>
+        )
+    }
+
+}
