dfeadf87872d260dc69b5eafdb0f714cc7b2edda
himpskatti <sofia.piekkola@hotmail.com>
2017-05-24 02:43:47 +0300

    Add carousel to react

--- frontend/shop/src/index.js
+++ frontend/shop/src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import TopNavComponent from './components/TopNavComponent';
 import BallShop from "./components/BallShop";
+import CarouselComponent from "./components/CarouselComponent";
 
 ReactDOM.render(
     <TopNavComponent/>,
     document.getElementById("topNav")
+);
+
+ReactDOM.render(
+    <CarouselComponent/>,
+    document.getElementById("myCarousel")
 );
 
 ReactDOM.render(
