840c5f4d619c5d9257e0ec33dcbbf1f359897439
himpskatti <sofia.piekkola@hotmail.com>
2017-05-24 02:46:06 +0300

    Add all to react

--- frontend/shop/src/components/BallShop.js
+++ frontend/shop/src/components/BallShop.js
@@ -2,12 +2,18 @@ import React from 'react';
 import Content from "./Content";
 import FooterComponent from "./FooterComponent";
 import MiddleNavComponent from "./MiddleNavComponent";
+import TopNavComponent from "./TopNavComponent";
+import CarouselComponent from "./CarouselComponent";
 
 export default class BallShop extends React.Component{
 
     render(){
         return(
             <div>
+                <div class="container">
+                    <TopNavComponent/>
+                </div>
+                <CarouselComponent/>
                 <div className="container-fullwidth">
                     <MiddleNavComponent/>
                 </div>
--- frontend/shop/src/index.js
+++ frontend/shop/src/index.js
@@ -1,18 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TopNavComponent from './components/TopNavComponent';
 import BallShop from "./components/BallShop";
-import CarouselComponent from "./components/CarouselComponent";
-
-ReactDOM.render(
-    <TopNavComponent/>,
-    document.getElementById("topNav")
-);
-
-ReactDOM.render(
-    <CarouselComponent/>,
-    document.getElementById("myCarousel")
-);
 
 ReactDOM.render(
     <BallShop/>,
