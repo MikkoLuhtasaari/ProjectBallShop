8d0be5845099157b551c34e0ab64c554c9a14f8f
himpskatti <sofia.piekkola@hotmail.com>
2017-05-24 01:04:32 +0300

    Add Top navigation to react

--- frontend/shop/src/components/TopNavComponent.js
+++ frontend/shop/src/components/TopNavComponent.js
@@ -15,15 +15,28 @@ export default class TopNavComponent extends React.Component{
 
     render(){
         return(
-            <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <form className="navbar-form navbar-left form-horizontal" role="search">
-                    <SearchComponent />
-                </form>
-                <ul className="nav navbar-nav navbar-right">
-                    <LoginComponent/>
-                    <ShoppingCartComponent/>
-                </ul>
-            </div>
+            <nav className="navbar navbar-inverse navbar-fixed-top bs-dark">
+                <div className="navbar-header">
+                    <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                        <span className="sr-only">Toggle navigation</span>
+                        <span className="icon-bar"/>
+                        <span className="icon-bar"/>
+                        <span className="icon-bar"/>
+                    </button>
+                    <a className="navbar-brand" href="#">
+                        <img src="images/logo.png" alt="Bally Balls" id="logoImg"/>
+                    </a>
+                </div>
+                <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <form className="navbar-form navbar-left form-horizontal" role="search">
+                        <SearchComponent />
+                    </form>
+                    <ul className="nav navbar-nav navbar-right">
+                        <LoginComponent/>
+                        <ShoppingCartComponent/>
+                    </ul>
+                </div>
+            </nav>
         )
     }
 }
