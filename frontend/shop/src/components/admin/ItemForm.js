583abf4b136c84eb23c7a8f91bf74005ead13c34
himpskatti <sofia.piekkola@hotmail.com>
2017-05-25 02:56:02 +0300

    Add required annotations

--- frontend/shop/src/components/admin/ItemForm.js
+++ frontend/shop/src/components/admin/ItemForm.js
@@ -43,47 +43,47 @@ export default class ItemForm extends React.Component{
         <form id="myForm" style={styles.form} action={this.state.formAction}>
           <div style={styles.row}>
             <div style={styles.title}>Name</div>
-            <input id="name" style={styles.input} type="text"/>
+            <input id="name" style={styles.input} type="text" required={true}/>
           </div>
           <div style={styles.row}>
             <div style={styles.title}>Color</div>
-            <input id="color" style={styles.input} type="text"/>
+            <input id="color" style={styles.input} type="text" required={true}/>
           </div>
           <div style={styles.row}>
             <div style={styles.title}>Diameter</div>
-            <input id="diameter" style={styles.input} type="number"/>
+            <input id="diameter" style={styles.input} type="number" required={true}/>
           </div>
           <div style={styles.row}>
             <div style={styles.title}>Weight</div>
-            <input id="weigth" style={styles.input} type="number"/>
+            <input id="weigth" style={styles.input} type="number" required={true}/>
           </div>
           <div style={styles.row}>
             <div style={styles.title}>Details</div>
-            <input id="details" style={styles.input} type="text"/>
+            <input id="details" style={styles.input} type="text" required={true}/>
           </div>
           <div style={styles.row}>
             <div style={styles.title}>Material</div>
-            <input id="material" style={styles.input} type="text"/>
+            <input id="material" style={styles.input} type="text" required={true}/>
           </div>
           <div style={styles.row}>
             <div style={styles.title}>Manufacturer</div>
-            <input id="manufacturer" style={styles.input} type="text"/>
+            <input id="manufacturer" style={styles.input} type="text" required={true}/>
           </div>
           <div style={styles.row}>
             <div style={styles.title}>Short Details</div>
-            <input id="shortDetails" style={styles.input} type="text"/>
+            <input id="shortDetails" style={styles.input} type="text" required={true}/>
           </div>
           <div style={styles.row}>
             <div style={styles.title}>Type</div>
-            <input id="type" style={styles.input} type="text"/>
+            <input id="type" style={styles.input} type="text" required={true}/>
           </div>
           <div style={styles.row}>
             <div style={styles.title}>Price</div>
-            <input id="price" style={styles.input} type="number"/>
+            <input id="price" style={styles.input} type="number" required={true}/>
           </div>
           <div style={styles.row}>
             <div style={styles.title}>Amount</div>
-            <input id="amount" style={styles.input} type="number"/>
+            <input id="amount" style={styles.input} type="number" required={true}/>
           </div>
           <input style={styles.submitButton} onClick={() => this.sendDataToDatabase(this.state.html)} type="submit"/>
         </form>
@@ -93,7 +93,7 @@ export default class ItemForm extends React.Component{
 //
 // <div style={styles.row}>
 // <div style={styles.title}>Image</div>
-// <input id="image" style={styles.input} type="file" accept="image/*"/>
+// <input id="image" style={styles.input} type="file" required={true} accept="image/*"/>
 // </div>
     sendDataToDatabase(http) {
         let container = document.getElementById('myForm');
