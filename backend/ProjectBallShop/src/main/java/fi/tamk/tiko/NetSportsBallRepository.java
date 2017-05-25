775ed98f57e6eb98f09af83428c0076d9e15402f
MikkoLuhtasaari <Mikko.Luhtasaari@gmail.com>
2017-05-25 18:27:31 +0300

    Add javadoc

--- backend/ProjectBallShop/src/main/java/fi/tamk/tiko/NetSportsBallRepository.java
+++ backend/ProjectBallShop/src/main/java/fi/tamk/tiko/NetSportsBallRepository.java
@@ -3,6 +3,13 @@ package fi.tamk.tiko;
 import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
+/**
+ * Contains net sports balls
+ *
+ * @author      Mikko Luhtasaari
+ * @version     4.0
+ * @since       1.0
+ */
 public interface NetSportsBallRepository extends CrudRepository<NetSportsBall , Long> {
     List<NetSportsBall> findByColor(String color);
     List<NetSportsBall> findByMaterial(String material);
--- backend/ProjectBallShop/src/main/java/fi/tamk/tiko/TargetSportsBallRepository.java
+++ backend/ProjectBallShop/src/main/java/fi/tamk/tiko/TargetSportsBallRepository.java
@@ -3,6 +3,13 @@ package fi.tamk.tiko;
 import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
+/**
+ * Contains target sports balls
+ *
+ * @author      Mikko Luhtasaari
+ * @version     4.0
+ * @since       1.0
+ */
 public interface TargetSportsBallRepository extends CrudRepository<TargetSportsBall, Long> {
     List<TargetSportsBall> findByColor(String color);
     List<TargetSportsBall> findByMaterial(String material);
